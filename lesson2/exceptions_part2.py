b2ae316e0657b6a8a4b333058165ca1b0b9478d4
AlinaPa <aliaska-92@mail.ru>
2019-03-02 13:47:04 +0300

    add lesson3

--- lesson2/exceptions_part2.py
+++ lesson2/exceptions_part2.py
@@ -2,8 +2,9 @@ def get_sum(num_one, num_two):
     try:
         les = int(num_one + num_two)
         print(les)
-    except ValueError:
+    except (ValueError, TypeError):
         print("Неверный тип даннных")
 
 
 get_sum("a", "b")
+get_sum("a", None)
